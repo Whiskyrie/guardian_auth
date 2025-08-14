651cd862244628dd2865fadc3e38c233c2aafc08
Whiskyrie <evandroropfilho@gmail.com>
2025-08-14 14:58:12 -0300

    Refatora mutações de login e registro de usuário para melhorar a autenticação e tratamento de erros

--- app/graphql/mutations/login_user.rb
+++ app/graphql/mutations/login_user.rb
@@ -1,31 +1,31 @@
 module Mutations
-  class LoginUser < Mutations::BaseMutation
+  class LoginUser < BaseMutation
     argument :email, String, required: true
     argument :password, String, required: true
-
+    
     field :auth_payload, Types::AuthPayloadType, null: false
-
-    def self.resolve(email:, password:)
+    
+    def resolve(email:, password:)
       user = User.find_by(email: email)
-
+      
       if user&.authenticate(password)
         token = JwtService.encode(user_id: user.id)
         {
-          auth_payload:{
-            token :token,
-            user :user,
-            errors : []
-
+          auth_payload: {
+            token: token,
+            user: user,
+            errors: []
           }
         }
       else
         {
-          auth_payload:{
+          auth_payload: {
             token: nil,
             user: nil,
-            errors: ["Invalid email or password"]
+            errors: ["Senha ou email inválidos!"]
           }
         }
+      end
     end
   end
 end
--- app/graphql/mutations/register_user.rb
+++ app/graphql/mutations/register_user.rb
@@ -4,9 +4,9 @@ module Mutations
     argument :password, String, required: true
     argument :first_name, String, required: true
     argument :last_name, String, required: true
-
+    
     field :auth_payload, Types::AuthPayloadType, null: false
-
+    
     def resolve(email:, password:, first_name:, last_name:)
       user = User.new(
         email: email,
@@ -14,6 +14,7 @@ module Mutations
         first_name: first_name,
         last_name: last_name
       )
+      
       if user.save
         token = JwtService.encode(user_id: user.id)
         {
@@ -24,13 +25,57 @@ module Mutations
           }
         }
       else
+        formatted_errors = format_user_errors(user.errors)
         {
           auth_payload: {
             token: nil,
             user: nil,
-            errors: user.errors.full_messages
+            errors: formatted_errors
           }
         }
+      end
+    end
+
+    private
+
+    def format_user_errors(errors)
+      errors.map do |attribute, message|
+        error_code = generate_error_code(attribute, message)
+        {
+          field: attribute.to_s,
+          message: message,
+          code: error_code
+        }
+      end
+    end
+
+    def generate_error_code(attribute, message)
+      case attribute
+      when :email
+        case message
+        when /can't be blank/i then 'EMAIL_REQUIRED'
+        when /has already been taken/i then 'EMAIL_ALREADY_TAKEN'
+        when /is invalid/i then 'EMAIL_INVALID'
+        else 'EMAIL_ERROR'
+        end
+      when :password
+        case message
+        when /can't be blank/i then 'PASSWORD_REQUIRED'
+        when /too short/i then 'PASSWORD_TOO_SHORT'
+        else 'PASSWORD_ERROR'
+        end
+      when :first_name
+        case message
+        when /can't be blank/i then 'FIRST_NAME_REQUIRED'
+        else 'FIRST_NAME_ERROR'
+        end
+      when :last_name
+        case message
+        when /can't be blank/i then 'LAST_NAME_REQUIRED'
+        else 'LAST_NAME_ERROR'
+        end
+      else
+        'VALIDATION_ERROR'
       end
     end
   end
